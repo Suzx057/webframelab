1b618760fc3bac77276313b3bd31741926ae4ac8
Suzx057 <gartartzz@gmail.com>
2024-09-10 13:32:30 +0700

    delete

--- lab8/lab8/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('items/', views.get_items, name='get_items'),
-    path('items/add/', views.add_item, name='add_item'),
-    path('items/list/', views.item_list, name='item_list'),
-]
